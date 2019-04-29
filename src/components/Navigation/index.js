e751d48bfc076789e819221ee39a5b1463d10b52
Jonathan Mooney <moonejon@gmail.com>
2019-04-28 22:48:47 -0500

    router change

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 function Navigation() {
   return (
       <Navbar bg='light'>
-        <Navbar.Brand href='#home'>Google Books Machine</Navbar.Brand>
+        <Navbar.Brand href='/'>Google Books Machine</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Nav className="mr-auto">
           <Nav.Link href="/Search">Search</Nav.Link>
