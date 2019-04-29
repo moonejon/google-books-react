05243a8c4087a49585f025ffec57bff4447a009d
Jonathan Mooney <moonejon@gmail.com>
2019-04-28 21:47:20 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Navbar, Nav } from 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <Navbar bg='light'>
+        <Navbar.Brand href='#home'>Google Books Machine</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Search</Nav.Link>
+          <Nav.Link href="#link">Saved</Nav.Link>
+          </Nav>
+      </Navbar>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
